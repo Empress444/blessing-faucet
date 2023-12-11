bd5dd239ec80546912e9603889e061377306f267
Empress444 <153399478+Empress444@users.noreply.github.com>
2023-12-11 10:29:44 -0800

    Update 444.sol

--- 444.sol
+++ 444.sol
@@ -1,1 +1,30 @@
+// SPDX-License-Identifier: MIT
+//
+//         :::         :::         :::
+//       :+:         :+:         :+:
+//     +:+ +:+     +:+ +:+     +:+ +:+
+//   +#+  +:+    +#+  +:+    +#+  +:+
+// +#+#+#+#+#+ +#+#+#+#+#+ +#+#+#+#+#+
+//      #+#         #+#         #+#
+//     ###         ###         ###
+//
+//   bless up, anon
+//   official: x.com/444token
 
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract FourFourtyFourToken is ERC20, Ownable {
+    // Constructor: Mint initial supply to the contract deployer
+    constructor() ERC20("444", "444") Ownable(msg.sender) {
+        uint256 initialSupply = 444444444444 * 1e18; // 444,444,444,444 tokens with 18 decimals
+        _mint(msg.sender, initialSupply);
+    }
+
+    // Function to renounce the contract ownership
+    function renounceContract() external onlyOwner {
+        renounceOwnership();
+    }
+}
